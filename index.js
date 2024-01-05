2f988ef306ff6ee36c17b4d3f0693493e6333844
codethelabs <codethelabs@gmail.com>
2024-01-05 10:35:47 +0300

    fixed the config file

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const db = require('./db');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const path = require('path');
 
 const app = express();
 
 // Use body-parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.get('*');
 
 // Include routes
 app.use('/api', routes);
